b17c6a37fb1d6794abbc4fe79bbafa9eeb6000ef
Sujin Panicker <spanic@codeaurora.org>
2021-08-31 18:57:18 +0530

    denniz: props: Fix rank of SW OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I49d8917aaaaeb58fe67dc2aaa4a80181eb4ab08b
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,8 @@
+# OMX
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 
+
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
