9bef17f63646f8511af718eea25b07cddc0b194a
Ethan Chen <intervigil@gmail.com>
2021-08-31 18:31:25 +0530

    denniz: RCSService: Build RCS packages
    Change-Id: I055e0ce92c2d2e8764918151be893424b497310a
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# RcsService
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling 
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
