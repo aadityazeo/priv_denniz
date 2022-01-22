5b621e3fb67c6b59c0a19f069fa69748bf94e73f
Giovix92 <ggualtierone@gmail.com>
2022-01-22 23:36:06 +0100

    denniz: Define boot animation and screen size

--- device.mk
+++ device.mk
@@ -14,6 +14,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
