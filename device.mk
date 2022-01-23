50b227028c5c96bdab600eefc22a79cba342361c
Giovix92 <ggualtierone@gmail.com>
2022-01-23 02:19:41 +0100

    denniz: vendor_overlay: Use PRODUCT_TARGET_VNDK_VERSION instead of hardcoding the path
    Change-Id: Ie9f484cf8916eb7e8fc4dc6016f39b316570f167

--- device.mk
+++ device.mk
@@ -54,4 +54,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vendor overlay
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor_overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor_overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION))
