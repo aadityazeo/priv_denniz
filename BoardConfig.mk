86177fd5f5e92dc35f9ecdbad1288dc17c16494b
Giovix92 <ggualtierone@gmail.com>
2022-02-02 16:41:35 +0100

    denniz: Always reboot on recovery if fatal reboot
    Change-Id: Id1aa72f5797ccc78473965feec1c016f0073dbe8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,7 @@ BOARD_RAMDISK_OFFSET := 0x11088000
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
