hypercleric/device_oneplus_denniz

denniz: Fix light sensor
- Replace proprietary sensor type with standard one

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

sensors/HalProxy.cpp
@@ -480,6 +480,10 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
+                    if (sensor.typeAsString == "qti.sensor.wise_light") {
+                        sensor.type = SensorType::LIGHT;
+                        sensor.typeAsString = "";
+                    }
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }

aadityazeo/priv_denniz

Change-Id: I217ba1d26ce8ed2b0be6e2b559be9544a78d96a6

sensors/HalProxy.cpp
@@ -480,6 +480,10 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
+                    if (sensor.typeAsString == "android.sensor.wise_light") {
+                        sensor.type = SensorType::LIGHT;
+                        sensor.typeAsString = "";
+                    }
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }